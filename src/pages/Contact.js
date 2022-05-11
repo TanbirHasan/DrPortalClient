56bf7ddab943240fd7f98cc6337c96a8884a8d36
unknown <mdtanbir576@gmail.com>
2022-05-11 15:54:59 +0600

    creating primary structure

---
+++ src/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h3>This is contact</h3>
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/pages/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <div>
+            <h3>This is reviews</h3>
+        </div>
+    );
+};
+
+export default Reviews;
